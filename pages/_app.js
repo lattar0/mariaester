989c56970baf280715efe454be97e8249e9d52d0
DerpyXD <34352268+DerpyXD@users.noreply.github.com>
2021-08-29 13:29:55 -0300

    desfazendo macaco coisas

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,9 @@
 import '../styles/globals.scss';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Maria Ester</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
 export default MyApp;
